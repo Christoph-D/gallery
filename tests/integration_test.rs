a8548da1dab241eeb024022bbafe571c8ba6c573
Christoph Dittmann <github@yozora.eu>
2021-08-22 00:26:57 +0200

    Add integration tests

---
+++ tests/integration_test.rs
@@ -0,0 +1,68 @@
+//! Integration tests.
+//!
+//! The tests here require imagemagick (the `convert` binary) to be in the `PATH`
+//! or the tests will fail when trying to generate thumbnails.
+use std::{fs, path::Path};
+
+/// A valid 1-pixel sized webp image.
+/// The image needs to be valid to test the generation of thumbnails.
+const DUMMY_WEBP: &[u8] = include_bytes!("dummy.webp");
+
+fn run_main(inputdir: &Path, outputdir: &Path, page_title: &str, footer: &str) {
+    if let Err(e) = gallery::run_on_args([
+        "gallery", // name of the binary, doesn't matter
+        &("--page_title=".to_owned() + page_title),
+        &("--footer=".to_owned() + footer),
+        &("--input=".to_owned() + inputdir.to_str().unwrap()),
+        &("--output=".to_owned() + outputdir.to_str().unwrap()),
+    ]) {
+        panic!("Error: {:?}", e)
+    }
+}
+
+#[test]
+fn test_empty_input() {
+    let tempdir_raw = tempfile::tempdir().unwrap();
+    let tempdir = tempdir_raw.path();
+    let inputdir = &tempdir.join("input");
+    let outputdir = &tempdir.join("output");
+
+    fs::create_dir(tempdir.join("input")).unwrap();
+
+    run_main(inputdir, outputdir, "Some title", "Some footer <a>link</a>");
+
+    let index = fs::read_to_string(outputdir.join("index.html")).unwrap();
+    assert!(index.contains("Some title"));
+    assert!(index.contains("Some footer <a>link</a>"));
+}
+
+#[test]
+fn test_simple_input() {
+    let tempdir_raw = tempfile::tempdir().unwrap();
+    let tempdir = tempdir_raw.path();
+    let inputdir = &tempdir.join("input");
+    let outputdir = &tempdir.join("output");
+
+    fs::create_dir(inputdir).unwrap();
+    fs::create_dir(inputdir.join("2021-01-01 Fuji, Japan")).unwrap();
+    fs::write(
+        inputdir.join("2021-01-01 Fuji, Japan/Summit.webp"),
+        DUMMY_WEBP,
+    )
+    .unwrap();
+
+    run_main(inputdir, outputdir, "Title", "Footer");
+
+    // The overview page should reference the image.
+    let index = fs::read_to_string(outputdir.join("index.html")).unwrap();
+    assert!(index.contains("Fuji, Japan"));
+    assert!(index.contains("Summit"));
+    assert!(index.contains("href=\"2021-01-01-fuji-japan/summit.webp\""));
+
+    assert!(outputdir
+        .join("2021-01-01-fuji-japan/summit.webp")
+        .is_file());
+    assert!(outputdir
+        .join("thumbnails/small/2021-01-01-fuji-japan/summit.webp")
+        .is_file());
+}
